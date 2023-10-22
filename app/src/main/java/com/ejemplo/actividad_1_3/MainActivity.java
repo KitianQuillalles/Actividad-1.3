0f1a35961a912b141e34b213052633bb7522f133
brodriguez <bastian3967a@gmail.com>
2023-10-22 20:46:00 -0300

    end point

--- app/src/main/java/com/ejemplo/actividad_1_3/MainActivity.java
+++ app/src/main/java/com/ejemplo/actividad_1_3/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ejemplo.actividad_1_3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -11,18 +12,23 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager lManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recycler_contactos);
-
-
         List<Contactos> items = new ArrayList<>();
-        items.add(new Contactos(1, "Juan", "+56977623282", "juan@gmail.com"));
-        items.add(new Contactos(2, "Fernando", "+56987652390", "fernando@gmail.com"));
-        items.add(new Contactos(3, "Felipe", "+56988542300", "felipe@gmail.com"));
+        items.add(new Contactos(R.drawable.ic_launcher_foreground, "Juan", "+56977623282", "juan@gmail.com"));
+        items.add(new Contactos(R.drawable.ic_launcher_foreground, "Fernando", "+56987652390", "fernando@gmail.com"));
+        items.add(new Contactos(R.drawable.ic_launcher_foreground, "Felipe", "+56988542300", "felipe@gmail.com"));
+
+        recyclerView = findViewById(R.id.recycler_contactos);
+        recyclerView.setHasFixedSize(true);
+
+        lManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(lManager);
 
         contactAdapter adapter = new contactAdapter(items);
         recyclerView.setAdapter(adapter);
